f2d5652194c0a9566ea061777f716f941c1f0d23
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-08-14 23:26:43 +0530

    Update Routes.jsx

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ScrollTop from "./ScrollTop";
 import Header from "../components/Header";
-import Home from "../routes/pages/Home";
+import Home from "./pages/Home";
 import Events from "./pages/Events";
 import News from "./pages/News";
 import NewView from "./pages/NewView";
