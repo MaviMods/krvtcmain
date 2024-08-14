69c5d17974b328931ab084b6bfb3bc20e1977653
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-08-14 19:11:22 +0000

    Update Notfound.jsx

--- src/routes/pages/Notfound.jsx
+++ src/routes/pages/Notfound.jsx
@@ -3,7 +3,7 @@ import EmptyData from "../../components/EmptyData";
 
 const Notfound = () => {
     document.title = TITLE + " | 404";
-    return <EmptyData msj={"No se encontró la página que buscas"} />;
+    return <EmptyData msj={"The page you are looking for was not found"} />;
 };
 
 export default Notfound;
