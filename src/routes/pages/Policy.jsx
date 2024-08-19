da42463c2b290702cc0abcb40a89f40f0ecf61f4
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-08-19 17:51:58 +0530

    Create Policy.jsx

---
+++ src/routes/pages/Policy.jsx
@@ -0,0 +1,15 @@
+import { TITLE } from "../../helpers/configs";
+import Slider from "../../components/Slider";
+import AttributesCard from "../../components/PolicyCard";
+
+const Policy = () => {
+    document.title = TITLE + " | Policy";
+    return (
+        <div>
+            <Slider />
+            <PolicyCard />
+        </div>
+    );
+};
+
+export default Policy;
