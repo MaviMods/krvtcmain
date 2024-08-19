31c9d66336c6d2c740a6d11ff9ef2de896c8641c
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-08-19 13:28:20 +0000

    Update PolicyCard.jsx

--- src/components/PolicyCard.jsx
+++ src/components/PolicyCard.jsx
@@ -101,7 +101,7 @@ const PolicyCard = () => {
     };
 
     const renderAttributesP2 = () => {
-        const divAtributes = Atributes.slice(3, 5);
+        const divAtributes = Atributes.slice(3, 9);
         return (
             <div className="grid grid-cols-1 sm:grid-cols-2">
                 {divAtributes.map((atribute, index) => {
